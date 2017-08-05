7220c6dc15f439a51f5f0d2e2e5cf27f439c725f
Mikael Lepistö <mikael.lepisto@vincit.fi>
2017-08-05 13:47:36 +0300

    Test for ignoring certain tables from truncate

--- tests/migrations/20141024070315_test_schema.js
+++ tests/migrations/20141024070315_test_schema.js
@@ -1,11 +1,21 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('User', function(table) {
-    table.bigincrements('id').primary();
-    table.string('username').index().unique().notNullable();
-    table.string('email');
-  });
+  return knex.schema
+    .createTable('User', function(table) {
+      table.bigincrements('id').primary();
+      table.string('username').index().unique().notNullable();
+      table.string('email');
+    })
+    .createTable('Ignoreme', function(table) {
+      table.bigincrements('id').primary();
+      table.string('description');
+    })
+    .then(function () {
+      return knex('Ignoreme').insert({ description: 'wat' });
+    });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('User');
+  return knex.schema
+    .dropTable('User')
+    .dropTable('Ignoreme');
 };
