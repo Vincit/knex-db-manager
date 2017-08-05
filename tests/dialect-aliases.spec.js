8647f4900e9ae0cee00bc6c4e9261682bb6aaebf
Mikael Lepistö <mikael.lepisto@vincit.fi>
2017-08-05 14:16:15 +0300

    Test supported dialect aliases

---
+++ tests/dialect-aliases.spec.js
@@ -0,0 +1,36 @@
+const dbManagerFactory = require('../lib').databaseManagerFactory;
+const expect = require('chai').expect;
+
+describe('Testing dialect aliases', function() {
+
+  const aliases = [
+    'pg', 'postgres', 'postgresql',
+    'mysql', 'maria', 'mariadb', 'mariasql',
+    'sqlite', 'sqlite3'
+  ];
+
+  aliases.forEach(dialect => {
+    it(`should load client with alias "${dialect}"`, () => {
+      const manager = dbManagerFactory({
+        knex: {
+          client: dialect
+        }
+      })
+    });
+  });
+
+  it(`should fail loading unknown client`, () => {
+    try {
+      const manager = dbManagerFactory({
+        knex: {
+          client: 'foobar'
+        }
+      });
+      expect('should have thrown an exception').to.equal(false);
+    } catch (err) {
+      expect(err.message).to.contains('is not supported');
+    }
+  });
+
+});
+
